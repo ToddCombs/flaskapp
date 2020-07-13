06ff10b3040219337d46a1b613766a53bfd75162
fraulen.shen <fraulen.shen@gmail.com>
2020-07-13 21:06:02 +0800

    消息提示与异常处理

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, flash, abort
 from models import User
 app = Flask(__name__)
+# 使用消息提示时需调用secret_key，flask使用他对消息加密
+app.secret_key = '213'
 
 @app.route('/')
 def hello_world():
     # return '<h1>Hello World!</h1>'
-    content = "Hello FFFFlask!"
-    return render_template("index.html", content=content)
+    # content = "Hello FFFFlask!"
+    # flash消息提示
+    flash("Helloooo Flask!")
+    # return render_template("index.html", content=content)
+    return render_template("index.html")
 
 @app.route('/user')
 # def hello_user():
 #     return 'Hellooooo user'
+# /user?id=1
 def user_index():
     user = User(1, 'ToddCombs')
     return render_template("user_index.html", user=user)
@@ -46,6 +52,7 @@ def user_id(id):
     return 'hello user:' + id
 # 获取参数的常用方法
 @app.route('/query_user')
+# /query_user?id=1
 def query_user():
     id = request.args.get('id')
     return 'query user:' + id
@@ -53,6 +60,43 @@ def query_user():
 @app.route('/query_url')
 def query_url():
     return 'query url:' + url_for('query_user')
+
+
+# 消息提示相关逻辑
+@app.route('/login', methods=['POST'])
+def login():
+    """获取用户输入的用户名和密码"""
+    form = request.form
+    username = form.get('username')
+    password = form.get('password')
+    # 判断逻辑
+    if not username:
+        flash("请输入用户名")
+        return render_template("index.html")
+    if not password:
+        flash("请输入密码")
+        return render_template("index.html")
+    if username == 'toddcombs' and password == '111111':
+        flash("登陆成功")
+        return render_template("index.html")
+    else:
+        flash("用户名/密码错误")
+        return render_template("index.html")
+
+# 异常提示信息的装饰器，可以让404错误返回更友好
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+# 异常处理：获取用户id如果=1，则返回正常页面，否泽调用abort函数返回指定404页
+# /use/1
+@app.route('/use/<use_id>')
+def use(use_id):
+    if int(use_id) == 1:
+        return render_template("use.html")
+    else:
+        # abort()函数用于提前退出一个请求，并用指定的错误码返回。
+        abort(404)
+
 
 if __name__ == '__main__':
     app.run()
